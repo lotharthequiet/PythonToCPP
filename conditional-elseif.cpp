bde6cfd58e93486aa84be8b0f0705e35630de272
Lothar TheQuiet <104657079+lotharthequiet@users.noreply.github.com>
2022-10-06 16:32:24 -0400

    Create conditional-elseif.cpp

---
+++ conditional-elseif.cpp
@@ -0,0 +1,25 @@
+ //Shows how to put conditional statements together,
+ //specfically putting "else if" after an "if" statement.
+ #include <iostream>
+ using namespace std;
+
+ int main() {
+
+     int grade = 85;
+
+     if (grade < 60) {
+         cout<<'F'<<endl;
+     }
+     else if (grade < 70) {
+         cout<<'D'<<endl;
+     }
+     else if (grade < 80) {
+         cout<<'C'<<endl;
+     }
+     else if (grade < 90) {
+         cout<<'B'<<endl;
+     }
+     else  cout<<'A'<<endl;
+
+     return 0;
+ }
